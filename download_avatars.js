0b6641865ae723783024bb20d97028b0b901783e
Majid <mohammadmajid55@gmail.com>
2019-04-09 01:15:50 +0000

    done printing avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,32 @@
 var request = require('request');
+var fs = require("fs");
+var token = require("./secrets.js")
+
 
 console.log("Welcome to the Github Avatar Downloader");
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var options = {
+  url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers: {
+    'User-Agent': 'request',
+  }
+};
+
+  request(options, function(err, res, body) {
+    var data = JSON.parse(body)
+    cb(err, body);
+
+    data.forEach(function(item) {
+      console.log(item.avatar_url)
+    });
+
+
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});

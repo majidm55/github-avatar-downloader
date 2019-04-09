9f263dcb7c7b1aaa5305ac0f935c4ebd001e8951
Majid <mohammadmajid55@gmail.com>
2019-04-09 15:37:14 +0000

    done getting images

--- download_avatars.js
+++ download_avatars.js
@@ -10,23 +10,32 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
   url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
   headers: {
-    'User-Agent': 'request',
+    'User-Agent': 'request'
   }
 };
 
+   function downloadImageByURL(url, filePath) {
+   request.get(url)
+     .on("error", function (err) {
+     throw err;
+   })
+   .pipe(fs.createWriteStream(filePath));
+
+ }
+
   request(options, function(err, res, body) {
-    var data = JSON.parse(body)
-    cb(err, body);
+    var data = JSON.parse(body);
+    cb(err, data);
 
     data.forEach(function(item) {
-      console.log(item.avatar_url)
-    });
-
-
+    downloadImageByURL(item.avatar_url, 'avatar/' + item.login + '.jpg');
+    console.log(item.avatar_url);
   });
+});
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
 });
